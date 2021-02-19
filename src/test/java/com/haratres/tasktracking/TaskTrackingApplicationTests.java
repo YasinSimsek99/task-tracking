a5d6219daf49c235c7a9b2c28d37aaaad148aeba
YasinSimsek99 <yasinsimsekk67@gmail.com>
2021-02-19 23:14:00 +0300

    starter

---
+++ src/test/java/com/haratres/tasktracking/TaskTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haratres.tasktracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
